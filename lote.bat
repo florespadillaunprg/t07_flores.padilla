d35be124486a388a66e62fbdf208d5dd2e7a5c3c
florespadillaunprg <57303134+florespadillaunprg@users.noreply.github.com>
2019-11-24 16:06:38 -0500

    Create lote.bat

---
+++ lote.bat
@@ -0,0 +1,88 @@
+python flores/iteracion/ejercicio01.py 20
+
+python flores/iteracion/ejercicio02.py 13
+
+python flores/iteracion/ejercicio03.py 50
+
+python flores/iteracion/ejercicio04.py 100
+
+python flores/iteracion/ejercicio05.py 10000
+
+
+python flores/mientras/ejercicio01.py "De lo mejor"
+
+python flores/mientras/ejercicio02.py 71.56
+
+python flores/mientras/ejercicio03.py 456.98
+
+python flores/mientras/ejercicio04.py "Adrian"
+
+python flores/mientras/ejercicio05.py 854.6
+
+
+python flores/para/ejercicio01.py "a" "l" "e" "x"
+
+python flores/para/ejercicio02.py "d" "m" "a" "i"
+
+python flores/para/ejercicio03.py "d" "m" "a"
+
+python flores/para/ejercicio04.py "n" "p" "m" "a"
+
+python flores/para/ejercicio05.py "c" "d" "p" "r" "n"
+
+
+python flores/rango/ejercicio01.py 200
+
+python flores/rango/ejercicio02.py 100 400
+
+python flores/rango/ejercicio03.py 500 5000
+
+python flores/rango/ejercicio04.py 4545 5454
+
+python flores/rango/ejercicio05.py 987 9870
+
+
+
+
+python olazabal/iteracion/iterador01.py 1598745
+
+python olazabal/iteracion/iterador02.py 50
+
+python olazabal/iteracion/iterador03.py 4500
+
+python olazabal/iteracion/iterador04.py 753
+
+python olazabal/iteracion/iterador05.py 23
+
+
+python olazabal/mientras/mientras01.py 60
+
+python olazabal/mientras/mientras02.py "lg"
+
+python olazabal/mientras/mientras03.py 1.36
+
+python olazabal/mientras/mientras04.py "anio de la lucha contra la corrupcion e impunidad"
+
+python olazabal/mientras/mientras05.py "Pedro"
+
+
+python olazabal/para/para01.py "a" "b" "c" "d"
+
+python olazabal/para/para02.py "a" "b" "c"
+
+python olazabal/para/para03.py "a" "b"
+
+python olazabal/para/para04.py "l" "n" "s"
+
+python olazabal/para/para05.py "a" "b" "c" "d" "e"
+
+
+python olazabal/rango/rango01.py 5 10
+
+python olazabal/rango/rango02.py 50 500
+
+python olazabal/rango/rango03.py 23 223
+
+python olazabal/rango/rango04.py 89 890
+
+python olazabal/rango/rango05.py 5000 50000
